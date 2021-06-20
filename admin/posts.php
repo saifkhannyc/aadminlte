5f6060e2d198c6444be0196e7ec208b6aab0c41f
Saif Khan <saifkhan6@gmail.com>
2021-06-19 23:39:18 -0400

    View, Add, Update Delete Posts Complete

--- admin/posts.php
+++ admin/posts.php
@@ -98,8 +98,30 @@ include 'inc/admin/menubar.php';
            }
            ?>
           </td>
-          <td scope="row"><?php echo $category_id; ?></td>
-          <td scope="row"><?php echo $author_id; ?></td>
+          <td scope="row">
+           <?php 
+            $query="SELECT* FROM category where cat_id='$category_id'";
+            $category_name=mysqli_query($dbc,$query);
+            while ($row = mysqli_fetch_array($category_name)){ 
+              $cat_id=$row['cat_id'];
+              $cat_name=$row['cat_name'];
+              echo $cat_name;
+            }
+          ?>
+
+          </td>
+          <td scope="row">
+           <?php 
+            $query="SELECT* FROM users where user_id='$user_id'";
+            $fullname=mysqli_query($dbc,$query);
+            while ($row = mysqli_fetch_array($fullname)){ 
+              $user_id=$row['user_id'];
+              $name=$row['fullname'];
+              echo $name;
+            }
+          
+          ?>
+          </td>
           <td scope="row"><?php echo $tags; ?></td>
           <td scope="row">
            <?php 
@@ -190,7 +212,17 @@ include 'inc/admin/menubar.php';
                $parent_id=$row['parent_id']; ?>
 
             <option value="<?php echo $cat_id; ?>"><?php echo $cat_name; ?></option>
-
+            <?php 
+             $query2="SELECT * FROM category where parent_id='$cat_id' order by cat_name ASC";
+             $child_cat=mysqli_query($dbc, $query2);
+             while ($row=mysqli_fetch_assoc($child_cat)) {
+                 $cat_id=$row['cat_id'];
+                 $cat_name=$row['cat_name'];
+              ?>
+            <option value="<?php echo $cat_id; ?>">--<?php echo $cat_name; ?></option>
+            <?php 
+             } 
+            ?>
             <?php  
              }
             ?>
@@ -271,101 +303,100 @@ include 'inc/admin/menubar.php';
        }
 
        else if ($do=="Edit"){ 
-        if(isset($_GET['u_id'])){ 
-          $u_id= $_GET['u_id'];
-          $query="SELECT * FROM users WHERE user_id='$u_id'";
-          $selectUser=mysqli_query($dbc,$query);
-          while ($row=mysqli_fetch_assoc($selectUser))
+        if(isset($_GET['p_id'])){ 
+          $p_id= $_GET['p_id'];
+          $query="SELECT * FROM posts WHERE post_id='$p_id'";
+          $selectPosts=mysqli_query($dbc,$query);
+          while ($row=mysqli_fetch_assoc($selectPosts))
           { 
-          $user_id   =$row['user_id'];
-          $image     =$row['image'];
-          $fullname  =$row['fullname'];
-          $username  =$row['username'];
-          $email     =$row['email'];
-          $phone     =$row['phone'];
-          $address   =$row['address'];
-          $status    =$row['status'];
-          $user_role =$row['user_role'];
-          $join_date =$row['join_date'];  
+          $post_id      =$row['post_id'];
+          $title        =$row['title'];
+          $description  =$row['description'];
+          $image        =$row['image'];
+          $category_id  =$row['category_id'];
+          $author_id    =$row['author_id'];
+          $tags         =$row['tags'];
+          $status       =$row['status'];
+          $date_posted  =$row['date_posted'];
             
             ?>
 
      <div class="card">
       <div class="card-header">
-       <h3 class="card-title">Update User Information</h3>
+       <h3 class="card-title">Update Post Information</h3>
       </div>
       <div class="card-body">
-       <!-- Add New User Form Starts -->
-       <form action="users.php?do=Update" method="POST" enctype="multipart/form-data">
+       <!-- Update Post Form Starts -->
+       <form action="posts.php?do=Update" method="POST" enctype="multipart/form-data">
         <div class="row">
-         <div class="col-lg-6">
+         <div class="col-lg-12">
           <div class="form-group">
-           <label for="fullname">Full Name</label>
-           <input type="text" name="fullname" id="" class="form-control" required autocomplete="off"
-            placeholder="Enter Fullname of the user" value="<?php echo $fullname; ?>">
-          </div>
-          <div class="form-group">
-           <label for="username">Username</label>
-           <input type="text" name="username" id="" class="form-control" required autocomplete="off"
-            placeholder="Enter username of the user" value="<?php echo $username; ?>">
-          </div>
-          <div class=" form-group">
-           <label for="email">Email</label>
-           <input type="email" name="email" id="" class="form-control" required autocomplete="off"
-            placeholder="Enter user email" value="<?php echo $email; ?>" title="Email cannot be changed" readonly>
-          </div>
-          <div class=" form-group">
-           <label for="password">Password</label>
-           <input type="password" name="password" id="" class="form-control" placeholder="Enter password">
+           <label for="title">Title</label>
+           <input type="text" name="title" id="" class="form-control" required autocomplete="off"
+            placeholder="Enter post title" value="<?php echo $title; ?>">
           </div>
           <div class="form-group">
-           <label for="re-password">Re-type Password</label>
-           <input type="password" name="re-password" id="" class="form-control" placeholder="Re-enter password">
-          </div>
-         </div>
+           <label for="title">Category</label>
+           <select name="category_id" class="form-control">
+            <option value="0">Please select category/sub-category</option>
+            <?php 
+             $query="SELECT * FROM category where parent_id=0 order by cat_name ASC";
+             $parent_cat=mysqli_query($dbc, $query);
+             while ($row=mysqli_fetch_assoc($parent_cat)){ 
+               $cat_id =$row['cat_id'];
+               $cat_name=$row['cat_name'];
+               $parent_id=$row['parent_id']; ?>
 
-         <div class="col-lg-6">
-          <div class="form-group">
-           <label for="phone">Phone no.</label>
-           <input type="text" name="phone" id="" class="form-control" required autocomplete="off"
-            placeholder="Enter phone number" value="<?php echo $phone; ?>">
-          </div>
-          <div class=" form-group">
-           <label for="address">Address</label>
-           <input type="text" name="address" id="" class="form-control" required autocomplete="off"
-            placeholder="Enter address" value="<?php echo $address; ?>">
-          </div>
-          <div class=" form-group">
-           <label for="status">User Status</label>
-           <select name="status" id="" class="form-control">
-            <option value="2">Please select the status</option>
-            <option value="1" <?php if($status==1){ echo 'selected'; } ?>>Active</option>
-            <option value="2" <?php if($status==2){ echo 'selected'; }?>>Inactive</option>
+            <option value="<?php echo $cat_id; ?>" <?php if($category_id==$cat_id) { echo 'selected';} ?>>
+             <?php echo $cat_name; ?></option>
+            <?php 
+             $query2="SELECT * FROM category where parent_id='$cat_id' order by cat_name ASC";
+             $child_cat=mysqli_query($dbc, $query2);
+             while ($row=mysqli_fetch_assoc($child_cat)) {
+                 $cat_id=$row['cat_id'];
+                 $cat_name=$row['cat_name'];
+              ?>
+            <option value="<?php echo $cat_id; ?>" <?php if($category_id==$cat_id) { echo 'selected';} ?>>
+             --<?php echo $cat_name; ?></option>
+            <?php 
+             } 
+            ?>
+            <?php  
+             }
+            ?>
            </select>
           </div>
           <div class="form-group">
-           <label for="user_role">User Role</label>
-           <select name="user_role" id="" class="form-control">
-            <option value="3">Please select the user role</option>
-            <option value="1" <?php if($user_role==1){ echo 'selected';}?>>Super Admin</option>
-            <option value="2" <?php if($user_role==2){ echo 'selected';}?>>Editor</option>
-            <option value="3" <?php if($user_role==3){ echo 'selected';}?>>User</option>
+           <label for="description">Description</label>
+           <textarea id="posts_desc" class="form-control" rows="4"
+            name="description"><?php echo $description; ?></textarea>
+          </div>
+          <div class="form-group">
+           <label for="tags">Meta Tags</label>
+           <input type="tags" name="tags" id="" class="form-control" required autocomplete="off"
+            placeholder="Enter post tags separated by commas" value="<?php echo $tags ?>">
+          </div>
+          <div class="form-group">
+           <label for="status">Post Status</label>
+           <select name="status" class="form-control">
+            <option value="0">Please select status</option>
+            <option value="1" <?php if($status==1){ echo'selected';}?>>Active</option>
+            <option value="2" <?php if($status==2){ echo'selected';}?>>Inactive</option>
            </select>
           </div>
           <div class="form-group">
-           <label for="image">Profile Image</label> <br>
+           <label for="image">Post thumbnail</label>
            <?php 
-           if(!empty($image))
+            if(!empty($image))
            { ?>
 
-           <img src="dist/img/users/<?php echo $image?>" alt="<?php echo $image?>" width="40">
+           <img src="dist/img/posts/<?php echo $image?>" alt="<?php echo $image?>" width="400" height="400"
+            style="display:block; margin-bottom:15px;">
 
            <?php 
            } else {
 
            echo "No Picture Uploaded";
-
-          
 
            }
            
@@ -374,10 +405,12 @@ include 'inc/admin/menubar.php';
            <input type="file" name="image" id="" class="form-control-file">
           </div>
          </div>
+
+
          <div class="col-lg-12 text-right">
           <a href="posts.php?do=Manage" class="btn btn-warning">Back </a>
-          <input type="hidden" name="updateUserID" value="<?php echo $user_id; ?>">
-          <input type="submit" name="update" id="" class="btn btn-primary" value="Update User">
+          <input type="hidden" name="updatePostID" value="<?php echo $post_id; ?>">
+          <input type="submit" name="update" id="" class="btn btn-primary" value="Update Post">
          </div>
         </div>
 
@@ -396,110 +429,47 @@ include 'inc/admin/menubar.php';
       // Update changes into the database
        else if ($do=="Update"){ 
         if(isset($_POST['update'])){ 
-          $updateUserID = $_POST['updateUserID'];
-          $fullname     =$_POST['fullname'];
-          $username     =$_POST['username'];
-          $email        =$_POST['email'];
-          $phone        =$_POST['phone'];
-          $address      =$_POST['address'];
-          $status       =$_POST['status'];
-          $user_role    =$_POST['user_role'];
-          // Get the Image File and Name
+          $updatePostID       = $_POST['updatePostID'];
+          $title              =mysqli_real_escape_string($dbc,$_POST['title']);
+          $category_id        =mysqli_real_escape_string($dbc,$_POST['category_id']);
+          $description        =mysqli_real_escape_string($dbc,$_POST['description']);
+          $tags               =mysqli_real_escape_string($dbc,$_POST['tags']);
+          $status             =mysqli_real_escape_string($dbc,$_POST['status']);
+          $author_id          =mysqli_real_escape_string($dbc,$_SESSION['user_id']);
+           // Get the Image File and Name
           $image        =$_FILES['image']['name'];
           // Get the Image File and Temporary Folder Name
           $image_tmp    =$_FILES['image']['tmp_name'];
-
-          $password     =$_POST['password'];
-          $repassword   =$_POST['re-password'];
-
-          if(!empty($password) && !empty($image)){ 
-
-            if($password==$repassword) { 
-              // hashed password
-              $hashedpass=sha1($password);
-              // Remove old image from the Folder
-              $removeQuery="SELECT * FROM users where user_id='$updateUserID'";
-              $removeImage= mysqli_query($dbc,$removeQuery);
+          if(!empty($image)){ 
+            $randomNumber=rand(0,9999999);
+            // Change Image Name
+            $imageFileName=$randomNumber.$image;
+            // Move uploaded file from temporary folder to destination Folder
+            move_uploaded_file($image_tmp, "dist/img/posts/".$imageFileName);
+            $deletePostThumbnail="SELECT * FROM posts where post_id='$updatePostID'";
+            $removeImage= mysqli_query($dbc, $deletePostThumbnail);
               while($row = mysqli_fetch_assoc($removeImage)){ 
                 $rImage= $row['image'];
-                unlink("dist/img/users/". $rImage);
+                unlink("dist/img/posts/". $rImage);
               }
-              // Change Image Name
-              $randomNumber=rand(0,9999999);
-              
-              $imageFileName=$randomNumber.$image;
-              // Move uploaded file from temporary folder to destination Folder
-              move_uploaded_file($image_tmp, "dist/img/users/".$imageFileName);
-              
-              // Update users data into the database 
-              $query= "UPDATE users SET fullname='$fullname',	username='$username' , email='$email', password='$hashedpass', phone='$phone', address='$address',	status='$status', user_role='$user_role', image='$imageFileName' where user_id='$updateUserID'";
-              $updateUser=mysqli_query($dbc,$query);
-              if($updateUser){ 
+            $query="UPDATE posts SET title='$title', description='$description', category_id='$category_id', author_id='$author_id', tags='$tags', status='$status', image='$imageFileName' where post_id='$updatePostID' ";
+            $updatePost=mysqli_query($dbc, $query);
+            if($updatePost){ 
 
-                header("Location:users.php?do=Manage");
+                header("Location:posts.php?do=Manage");
 
               } else { 
 
                 die("MySQL Database Error.". mysqli_error($dbc));
                 
               }
-            }
+            
+          } else{ 
+            $query="UPDATE posts SET title='$title', description='$description', category_id='$category_id', author_id='$author_id', tags='$tags', status='$status' where post_id='$updatePostID' ";
+            $updatePost=mysqli_query($dbc, $query);
+            if($updatePost){ 
 
-          } else if (!empty($password) && empty($image)) { 
-
-            if($password==$repassword) { 
-              // hashed password
-              $hashedpass=sha1($password);
-              
-              // Update users data into the database 
-              $query= "UPDATE users SET fullname='$fullname',	username='$username' , email='$email', password='$hashedpass', phone='$phone', address='$address',	status='$status', user_role='$user_role' where user_id='$updateUserID'";
-              $updateUser=mysqli_query($dbc,$query);
-              if($updateUser){ 
-
-                header("Location:users.php?do=Manage");
-
-              } else { 
-
-                die("MySQL Database Error.". mysqli_error($dbc));
-                
-              }
-            }
-
-          } else if (empty($password) && !empty($image)) { 
-
-            // Remove old image from the Folder
-              $removeQuery="SELECT * FROM users where user_id='$updateUserID'";
-              $removeImage= mysqli_query($dbc,$removeQuery);
-              while($row = mysqli_fetch_assoc($removeImage)){ 
-                $rImage= $row['image'];
-                unlink("dist/img/users/". $rImage);
-              }
-              // Change Image Name
-              $randomNumber=rand(0,9999999);
-              
-              $imageFileName=$randomNumber.$image;
-              // Move uploaded file from temporary folder to destination Folder
-              move_uploaded_file($image_tmp, "dist/img/users/".$imageFileName);
-              
-              // Update users data into the database 
-              $query= "UPDATE users SET fullname='$fullname',	username='$username' , email='$email', phone='$phone', address='$address',	status='$status', user_role='$user_role', image='$imageFileName' where user_id='$updateUserID'";
-              $updateUser=mysqli_query($dbc,$query);
-              if($updateUser){ 
-
-                header("Location:users.php?do=Manage");
-
-              } else { 
-
-                die("MySQL Database Error.". mysqli_error($dbc));
-                
-              }
-
-          } else if (empty($password) && empty($image)) { 
-             $query= "UPDATE users SET fullname='$fullname',	username='$username' , email='$email', phone='$phone', address='$address',	status='$status', user_role='$user_role' where user_id='$updateUserID'";
-              $updateUser=mysqli_query($dbc,$query);
-              if($updateUser){ 
-
-                header("Location:users.php?do=Manage");
+                header("Location:posts.php?do=Manage");
 
               } else { 
 
@@ -507,6 +477,12 @@ include 'inc/admin/menubar.php';
                 
               }
           }
+
+
+         
+          
+            
+           
         }
        }
 
